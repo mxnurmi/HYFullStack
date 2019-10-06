5c5bff47b58fa72fe220aabe91c42fa81874b2ad
Miska <miska.nurmi@helsinki.fi>
2019-10-06 19:02:06 +0300

    tehtava 1.9. tehty

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -10,7 +10,33 @@ const Button = ({ handleClick, text }) => (
 
 const Header = ({ text }) => <h1>{text}</h1>
 
-const Value = ({ text, value}) => <div><p1>{text} {value}</p1></div>
+const Value = ({ text, value, text2}) => <div>{text} {value} {text2}</div>
+
+const Statistics = ({good, neutral, bad}) => {
+
+    const all = () => good + neutral + bad
+    const avg = () => (good + bad*(-1)) / all()
+    const pos = () => (good / all())*100
+
+    if (all() === 0) {
+        return (
+          <div>
+            No feedback given
+          </div>
+        )
+      }
+
+    return (
+        <div>
+            <Value text='good' value={good} />
+            <Value text='neutral' value={neutral} />
+            <Value text='bad' value={bad} />
+            <Value text='all' value={all()} />
+            <Value text='average' value={avg()} />
+            <Value text='positive' value={pos()} text2='%' />
+        </div>
+    )
+  }
 
 const App = () => {
   // tallenna napit omaan tilaansa
@@ -28,9 +54,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
       <Button handleClick={() => setBad(bad + 1)} text='bad' />
       <Header text={stats} />
-      <Value text='good' value={good} />
-      <Value text='neutral' value={neutral} />
-      <Value text='bad' value={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} /> 
     </div>
   )
 }
