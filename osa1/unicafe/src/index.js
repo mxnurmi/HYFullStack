c9eff7e54ffa4fd5cf06cc2dbd16b7fa51012872
Miska <miska.nurmi@helsinki.fi>
2019-10-09 11:10:58 +0300

    unicafe valmis

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -2,42 +2,36 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import { constants } from 'fs';
 
-const Button = ({ handleClick, text }) => (
+
+
+const Header = ({ text }) => <h1>{text}</h1>
+
+const Value = ({ text, value }) => <tbody><tr><td>{text}</td><td>{value}</td></tr></tbody>
+
+const Statistics = ({text, value}) => {
+    return (
+            <Value text={text} value={value}/>
+    )
+  }
+
+  const Button = ({ handleClick, text }) => (
     <button onClick={handleClick}>
     {text}
     </button>
 )
-
-const Header = ({ text }) => <h1>{text}</h1>
-
-const Value = ({ text, value, text2}) => <div>{text} {value} {text2}</div>
-
-const Statistics = ({text, value}) => {
-
-    return (
-        <div>
-            <Value text={text} value={value} />
-
-            <Value text='all' value={all()} />
-            <Value text='average' value={avg()} />
-            <Value text='positive' value={pos()} text2='%' />
-        </div>
-    )
-  }
 
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
-  const feedback = 'give feedback'
-  const stats = 'statistics'
 
   const all = () => good + neutral + bad
   const avg = () => (good + bad*(-1)) / all()
   const pos = () => (good / all())*100
 
+  const feedback = 'give feedback'
+  const stats = 'statistics'
 
   if (all() === 0) {
     return (
@@ -58,12 +52,15 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text='good' />
       <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
       <Button handleClick={() => setBad(bad + 1)} text='bad' />
-      <Header text={stats} />
-{/*       <Statistics text= />  */}
-      <Value text='good' value={good} />
-      <Value text='neutral' value={neutral} />
-      <Value text='bad' value={bad} />
-
+      <Header text={stats} /> 
+      <table>
+        <Statistics text='good' value={good} />
+        <Statistics text='neutral' value={neutral} />
+        <Statistics text='bad' value={bad} />
+        <Statistics text='all' value={all()}/>
+        <Statistics text='average' value={avg() } />
+        <Statistics text='positive' value={pos().toString()+ ' %'} />
+      </table>
     </div>
   )
 }
