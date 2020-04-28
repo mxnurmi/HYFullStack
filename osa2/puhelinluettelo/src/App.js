c290de2e3879dd18493e235dc8cfa5becf217293
Miska <miska.nurmi@helsinki.fi>
2020-04-28 15:31:09 +0300

    2.10 done

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,15 +2,31 @@ import React, { useState } from 'react'
 
 const Person = ({ person }) => {
   return (
-    <div>{person.name}</div>
+    <div>{person.name} {person.number}</div>
   )
 }
 
+const Persons = ( {personsToShow} ) => (
+  personsToShow.map((person, i) =>
+    <Person key={person.name} person={person}/>
+  )
+)
+
+const Filter = ( {filterName, handleChange}) => (
+  <div>
+    filter shown with <input type="text" value={filterName} onChange={handleChange} />
+  </div>
+)
+
+
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState ('')
+  const [ filterName, setFilterName] = useState ('')
 
   const addNote = (event) => {
     event.preventDefault()
@@ -20,32 +36,41 @@ const App = () => {
       window.alert(`${newName} is already added to phonebook`)
     } else {
       const noteObject = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
       console.log(noteObject)
       
       setPersons(persons.concat(noteObject))
       setNewName('')
     }
-
+  
   }
+
+  const personsToShow = filterName === ''
+  ? persons
+  : persons.filter(person => (person.name.toLowerCase()).includes(filterName.toLowerCase()))
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter filterName={filterName} handleChange={(event) => setFilterName(event.target.value)} />
+      <h2>add a new</h2>
       <form onSubmit={addNote}>
         <div>
           name: <input value={newName} onChange={(event) => setNewName(event.target.value)}/>
+        </div>
+        <div> 
+          number: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      {persons.map((person, i) =>
-        <Person key={person.name} person={person}/>
-      )}
-       
+      <h3>Numbers</h3>
+
+      <Persons personsToShow={personsToShow} />
+
     </div>
   )
 }
