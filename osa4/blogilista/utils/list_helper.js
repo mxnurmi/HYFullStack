0b20612f90fa96aa32c9a596eb0a19e3dc5442cc
Miska <miska.nurmi@helsinki.fi>
2020-11-30 14:54:41 +0200

    4.7. done

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,24 +1,48 @@
+// const { max } = require('lodash')
+const lodash = require('lodash')
+
 const dummy = (blogs) => {
     return 1
   }
 
 const totalLikes = (blogs) => {
-  const reducer = (sum, blogItem) => sum + blogItem.likes
-
-  return blogs.reduce(reducer, 0)
+  const reduced = (sum, blogItem) => sum + blogItem.likes
+  const result = blogs.reduce(reduced, 0)
+  return result
 } 
 
 const favoriteBlog = (blogs) => {
-  const reducer = (max, cur) => Math.max(max, cur)
+  const reduced = (max, cur) => {
+    return Math.max(max, cur)
+  }
   likes = blogs.map(el => el.likes)
-  maximum = likes.reduce(reducer, -Infinity)
+  maximum = likes.reduce(reduced, -Infinity)
   bestBlog = blogs.find(el => el.likes === maximum)
-  console.log(bestBlog)
   return bestBlog
+}
+
+const mostBlogs = (blogs) => {
+
+  blogAmounts = lodash.countBy(blogs, 'author')
+  maximumBlogs = lodash.values(blogAmounts)
+  maxBlog = lodash.max(maximumBlogs)
+  
+  // console.log(blogAmounts)
+  // console.log(maximumBlogs)
+  // console.log(maxBlog)
+
+  for (const [key,value] of Object.entries(blogAmounts)) {
+    if(value === maxBlog) {
+      // console.log(key, value)
+      return {'author': key, 'blogs': value}
+    }
+  }
+
 }
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
