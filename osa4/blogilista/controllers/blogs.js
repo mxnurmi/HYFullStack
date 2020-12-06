b71f8c60b5ecded0237fd6883bf9e89aed207037
Miska <miska.nurmi@helsinki.fi>
2020-12-06 14:09:24 +0200

    4.19. done

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,23 +1,32 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogRouter.get('/', async (request, response) => {
-
   const blogs = await Blog
     .find({}).populate('user', {username: 1, name: 1, id: 1})
+
   response.json(blogs.map(blog => blog.toJSON()))
-    // Blog
-    //   .find({})
-    //   .then(blogs => {
-    //     response.json(blogs)
-    // })
 
 })
   
-blogRouter.post('/', async (request, response) => {
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
+blogRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   if(typeof body.likes === 'undefined') {
     body.likes = 0
@@ -26,8 +35,7 @@ blogRouter.post('/', async (request, response) => {
   if(typeof body.title === 'undefined' || typeof body.url === 'undefined') {
      response.status(400).end()
   } else {
-
-    const user = await User.findById(body.userId)
+    // const user = await User.findById(body.userId)
     const blog = new Blog(body)
     blog.user = user._id
     const result = await blog.save()
